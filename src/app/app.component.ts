469bc4f362d539f09f08c32d359d65e649b1bacd
Aresso93 <135212764+Aresso93@users.noreply.github.com>
2023-10-05 13:07:17 +0200

    esercizio

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'super-counter';
+
+  inc: any = {value: 0};
+  res: any = {value: 0};
+
+  increaseCalled(n:number){
+    console.log('INCREASE', n);
+    this.inc = {value:n}
+  }
+
+  resetCalled(n:number){
+    console.log('RESET', n);
+    this.res = {value:n};
+  }
+}
---
+++ src/app/services/counter.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CounterService {
+
+  constructor() { }
+
+  count = new BehaviorSubject<number>(0);
+
+  increaseCount(){
+    const actualCount = this.count.value;
+    const increasedCount = actualCount +1
+    this.count.next(increasedCount)
+  }
+
+  resetCount(){
+    this.count.next(0);
+
+  }
+}
